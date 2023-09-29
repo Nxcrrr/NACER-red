3050b19f06c1ead9f9b746529300eb8caf9491cc
Nxcrrr <145367420+Nxcrrr@users.noreply.github.com>
2023-09-29 08:45:40 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"fmt"
+	"projet-red-N/src/utils"
+	"time"
+)
+
+var p utils.Personnage
+
+func main() {
+	utils.AnimateText("Nacer présente ... Le Projet Red !")
+	fmt.Println("")
+	utils.AnimateText("Chargement ...")
+	time.Sleep(3 * time.Second)
+	p.Menu()
+}
